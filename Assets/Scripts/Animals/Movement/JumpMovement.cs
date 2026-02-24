023efbe0531bd3dabc93a1b310ec9eec999e8da3
MetelskyVsevolod <metelskyvsevolod@gmail.com>
2026-02-24 16:12:54 +0900

    Implemented jump movement

--- Assets/Scripts/Animals/Movement/JumpMovement.cs
+++ Assets/Scripts/Animals/Movement/JumpMovement.cs
@@ -1,24 +1,35 @@
 using System;
 using Animals.Core;
+using UnityEngine;
 
 namespace Animals.Movement
 {
     [Serializable]
     public class JumpMovement : MovementStrategyBase
     {
-        public override void OnInitialize(Animal animal)
-        {
-            throw new NotImplementedException();
-        }
+        [SerializeField] private float jumpForce = 5f;
+        [SerializeField] private float jumpInterval = 1.5f;
+
+        [NonSerialized] private float _timer;
+
+        public override void OnInitialize(Animal animal) => _timer = jumpInterval;
 
         public override void Tick(Animal animal)
         {
-            throw new NotImplementedException();
+            _timer += Time.deltaTime;
+            if (_timer < jumpInterval)
+            {
+                return;
+            }
+
+            _timer = 0f;
+            var dir = UnityEngine.Random.insideUnitCircle.normalized;
+            animal.RigidbodyComponent.AddForce(new Vector3(dir.x, 1f, dir.y) * jumpForce, ForceMode.Impulse);
         }
 
         public override void Reset()
         {
-            throw new NotImplementedException();
+            _timer = jumpInterval;
         }
     }
 }
